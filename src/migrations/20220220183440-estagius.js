50db14f000eff325fd85d10586f5233e545e443a
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-02-20 18:25:54 -0300

    categories migration

--- src/migrations/20220220183440-estagius.js
+++ src/migrations/20220220183440-estagius.js
@@ -1,18 +1,32 @@
-'use strict';
-import {Datatypes } from 'sequelize'
+'use strict'
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    id:{
-      type:Datatypes.String
-    }
+  async up (queryInterface, Sequelize){
+    return await queryInterface.createTable('categories',{
+      id:{
+        type:Sequelize.INTEGER,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true
+      },
+      name:{
+        type:Sequelize.STRING,
+        allowNull:false
+      },
+      created_at:{
+        type:Sequelize.DATE,
+        defaultValue:Sequelize.NOW
+      },
+      updated_at:{
+        type:Sequelize.DATE
+      }
+
+    })
+
+  
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+async down  (queryInterface, Sequelize) {
+  return await queryInterface.dropTable('categories')
   }
 };
